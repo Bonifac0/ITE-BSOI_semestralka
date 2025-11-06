a1ae519a38b3253e27684b1566a331c254d49b2f
Bonifac0 <kolenomi@seznam.cz>
2025-11-06 14:06:06 +0100

    add category to logs

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,12 @@
 from datetime import datetime
 
 
-def log(msg, level="INFO"):
+def log(msg, level="INFO", category=""):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"[{timestamp}] [{level}] {msg}")
+    cat = f"[{category}] " if category else ""
+    print(f"[{timestamp}] [{level}] {cat}{msg}")
+
+
+if __name__ == "__main__":
+    log("ssss", level="WARNING", category="AWS")
+    log("aaa")
